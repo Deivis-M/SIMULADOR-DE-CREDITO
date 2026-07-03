08f42b6f221867634b16e967e59ed7c3dafea5da
Deivis-M <nitaballenita09@gmail.com>
2026-07-03 16:30:26 -0500

    funcion calcularDisponible

--- funciones.js
+++ funciones.js
@@ -1,1 +1,8 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+function calcularDisponible(ingreso, egresos) {
+    let disponible = ingreso - egresos;
+    if (disponible < 0) {
+        disponible = 0;
+    }
+    return disponible;
+}
